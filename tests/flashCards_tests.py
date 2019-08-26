7085b724138a5741f0c21580dcc64654c148b9bf
RadCarl <carl.radcliff1@gmail.com>
2019-08-26 09:01:39 +0100

    Tests working for Site

--- tests/flashCards_tests.py
+++ tests/flashCards_tests.py
@@ -2,7 +2,8 @@ from nose.tools import *
 import sys
 from flashCards import translate_words
 
-# Tests go here
+translate_words.app.config['TESTING'] = True
+web = translate_words.app.test_client()
 
 # Valid translation
 def test_translation_via_googleTranslate():
@@ -16,9 +17,15 @@ def test_translation_via_googleTranslate():
 # Note: this test needs to be mocked once dev complete to avoid incurring api usage charges!!
 
 # test the web Form
-# def test_index():
-#     rv.web.get('/', follow_redirects=True)
-#     assert_equal(rv.status_code, 404)
-#
-#     rv.web.get('/translate', follow_redirects=True)
-#     assert_in(b"Enter a word for translation", rv.data)
+def test_index():
+
+    rv = web.get('/', follow_redirects=True)
+    assert_equal(rv.status_code, 404)
+
+    rv = web.get('/translate', follow_redirects=True)
+    assert_equal(rv.status_code, 200)
+    assert_in(b"Enter a word for translation", rv.data)
+
+    data = {'word': 'Hallo Welt'}
+    rv = web.post('/translate', follow_redirects=True, data = data)
+    assert_in(b"Hello World", rv.data)
